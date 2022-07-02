46e4440462cb447e0dd9823f1b47bfaafd070402
BigMike <peter@flynn.network>
2022-07-01 21:18:52 -0700

    Multi-threading

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { getAppAsync, getDefaultAppDependenciesAsync } from './app';
 import { defaultHttpServiceWithRateLimiterConfig } from './config';
 import { logger } from './logger';
 import { providerUtils } from './utils/provider_utils';
+const os = require("os");
+const cluster = require("cluster");
+
+const clusterWorkerSize = os.cpus().length;
 
 if (require.main === module) {
-    (async () => {
+    const mainLoop = async () => {
         const provider = providerUtils.createWeb3Provider(
             defaultHttpServiceWithRateLimiterConfig.ethereumRpcUrl,
             defaultHttpServiceWithRateLimiterConfig.rpcRequestTimeout,
@@ -12,7 +16,23 @@ if (require.main === module) {
         );
         const dependencies = await getDefaultAppDependenciesAsync(provider, defaultHttpServiceWithRateLimiterConfig);
         await getAppAsync(dependencies, defaultHttpServiceWithRateLimiterConfig);
-    })().catch((err) => logger.error(err.stack));
+    };
+
+    if (clusterWorkerSize > 1) {
+        if (cluster.isMaster) {
+            for (let i = 0; i < clusterWorkerSize; i++) {
+                cluster.fork();
+            }
+
+            cluster.on("exit", function (worker: any) {
+                console.log("Worker", worker.id, " has exited.")
+            })
+        } else {
+            mainLoop().catch((err) => logger.error(err.stack));
+        }
+    } else {
+        mainLoop().catch((err) => logger.error(err.stack));
+    }
 }
 process.on('uncaughtException', (err) => {
     logger.error(err);
